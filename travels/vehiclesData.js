fd861b6baf943072e7cdedac9e6336f3558baaa3
KRV-Nexa <krvnexa@gmail.com>
2025-09-15 22:43:14 +0530

    commit-1

---
+++ travels/vehiclesData.js
@@ -0,0 +1,31 @@
+// vehiclesData.js
+export const vehicles = [
+    {
+        name: "Swift Dezire",
+        seats: 5,
+        gps: true,
+        image: "cars/Swift Dezire.avif"
+
+    },
+     {
+        
+        name: "Maruti Ertiga",
+        seats: 7,
+        gps: true,
+        image: "cars/Maruti Ertiga.avif"
+
+    },
+     {
+        name: "Innova crysta",
+        seats: 6+1,
+        gps: true,
+       image: "cars/Innova crysta.avif"
+    },
+    {
+        name: "Innova crysta",
+        seats: 7+1,
+        gps: true,
+       image: "cars/Innova crysta.avif"
+    },
+   
+];
---
+++ travels/vehiclesluxary.js
@@ -0,0 +1,53 @@
+// vehiclesData.js
+export const vehicles = [
+    {
+        name: "Honda City",
+        seats: 5,
+        gps: true,
+        image: "cars/Honda City.avif"
+    },
+     {
+        name: "Toyota Camry",
+        seats: 5,
+        gps: true,
+        image: "cars/Toyota Camry.avif"
+    },
+     {
+        name: "Toyota Fortuner",
+        seats: 7,
+        gps: true,
+        image: "cars/Toyota Fortuner.avif"
+    },
+     {
+        name: "Mercedes-Benz C 250",
+        seats: 5,
+        gps: true,
+        image: "cars/Mercedes-Benz C 250.avif"
+    },
+     {
+        name: "bmw 75",
+        seats: 5,
+        gps: true,
+        image: "cars/bmw.avif"
+    },
+     {
+        name: "BMW 75 L",
+        seats: 5,
+        gps: true,
+        image: "cars/BMW GT.avif"
+    },
+     {
+        name: "Audi Q7",
+        seats: 7,
+        gps: true,
+        image: "cars/Audi Q7.avif"
+    },
+     {
+        name: "Bentley",
+        seats: 4,
+        gps: true,
+        image: "cars/Bentley.avif"
+    },
+
+   
+];
