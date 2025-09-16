0d75a45052b04e6a892bf2ccca4f493138703787
KRV-Nexa <krvnexa@gmail.com>
2025-09-16 15:06:50 +0530

    commit-2

---
+++ travels/carsData.js
@@ -0,0 +1,223 @@
+export const carsData = [
+  {
+    name: "Honda City",
+      image: "cars/Honda City.avif",
+    tariffs: {
+      regular: [
+        { label: "4 Hrs - 40 Kms", price: "2500" },
+        { label: "8 Hrs - 80 Kms", price: "5000" },
+        { label: "10 Hrs - 100 Kms", price: "7000" },
+        { label: "12 Hrs - 120 Kms", price: "8000" },
+        { label: "14 Hrs - 140 Kms", price: "9000" },
+        { label: "16 Hrs - 160 Kms", price: "10000" },
+        { label: "24 Hrs - 240 Kms", price: "12000" }
+      ],
+      day: [
+        { label: "12 Hrs", price: "5000" },
+        { label: "24 Hrs", price: "7500" },
+        { label: "Fuel Charge", price: "6 Km / 1 Litre" },
+        { label: "Chauffeur Allowance", price: "500" },
+        { label: "Extra Hr", price: "200" }
+      ],
+      outstation: [
+        { label: "Km limit / day", price: "400" },
+        { label: "Per Km Cost", price: "50" },
+        { label: "Chauffeur Allowance", price: "500" }
+      ]
+    }
+  },
+  {
+    name: "Camry",
+     image: "cars/Toyota Camry.avif",
+    tariffs: {
+      regular: [
+        { label: "4 Hrs - 40 Kms", price: "4000" },
+        { label: "8 Hrs - 80 Kms", price: "8000" },
+        { label: "10 Hrs - 100 Kms", price: "10000" },
+        { label: "12 Hrs - 120 Kms", price: "11000" },
+        { label: "14 Hrs - 140 Kms", price: "12000" },
+        { label: "16 Hrs - 160 Kms", price: "13000" },
+        { label: "24 Hrs - 240 Kms", price: "18000" }
+      ],
+      day: [
+        { label: "12 Hrs", price: "8000" },
+        { label: "24 Hrs", price: "12000" },
+        { label: "Fuel Charge", price: "6 Km / 1 Litre" },
+        { label: "Chauffeur Allowance", price: "750" },
+        { label: "Extra Hr", price: "300" }
+      ],
+      outstation: [
+        { label: "Km limit / day", price: "400" },
+        { label: "Per Km Cost", price: "70" },
+        { label: "Chauffeur Allowance", price: "750" }
+      ]
+    }
+  },
+  {
+    name: "Fortuner",
+    image: "cars/Toyota Fortuner.avif",
+    tariffs: {
+      regular: [
+        { label: "4 Hrs - 40 Kms", price: "6000" },
+        { label: "8 Hrs - 80 Kms", price: "12000" },
+        { label: "10 Hrs - 100 Kms", price: "14000" },
+        { label: "12 Hrs - 120 Kms", price: "16000" },
+        { label: "14 Hrs - 140 Kms", price: "18000" },
+        { label: "16 Hrs - 160 Kms", price: "20000" },
+        { label: "24 Hrs - 240 Kms", price: "22000" }
+      ],
+      day: [
+        { label: "12 Hrs", price: "12000" },
+        { label: "24 Hrs", price: "18000" },
+        { label: "Fuel Charge", price: "6 Km / 1 Litre" },
+        { label: "Chauffeur Allowance", price: "1000" },
+        { label: "Extra Hr", price: "400" }
+      ],
+      outstation: [
+        { label: "Km limit / day", price: "400" },
+        { label: "Per Km Cost", price: "50" },
+        { label: "Chauffeur Allowance", price: "1000" }
+      ]
+    }
+  },
+  {
+    name: "Mercedes-Benz C 250",
+     image: "cars/Mercedes-Benz C 250.avif",
+    tariffs: {
+      regular: [
+        { label: "4 Hrs - 40 Kms", price: "5000" },
+        { label: "8 Hrs - 80 Kms", price: "10000" },
+        { label: "10 Hrs - 100 Kms", price: "12500" },
+        { label: "12 Hrs - 120 Kms", price: "14000" },
+        { label: "14 Hrs - 140 Kms", price: "16500" },
+        { label: "16 Hrs - 160 Kms", price: "18000" },
+        { label: "24 Hrs - 240 Kms", price: "20000" }
+      ],
+      day: [
+        { label: "12 Hrs", price: "10000" },
+        { label: "24 Hrs", price: "15000" },
+        { label: "Fuel Charge", price: "6 Km / 1 Litre" },
+        { label: "Chauffeur Allowance", price: "750" },
+        { label: "Extra Hr", price: "750" }
+      ],
+      outstation: [
+        { label: "Km limit / day", price: "400" },
+        { label: "Per Km Cost", price: "70" },
+        { label: "Chauffeur Allowance", price: "1500" }
+      ]
+    }
+  },
+  {
+    name: "BMW 75",
+     image: "cars/bmw.avif",
+    tariffs: {
+      regular: [
+        { label: "4 Hrs - 40 Kms", price: "7500" },
+        { label: "8 Hrs - 80 Kms", price: "15000" },
+        { label: "10 Hrs - 100 Kms", price: "18000" },
+        { label: "12 Hrs - 120 Kms", price: "20000" },
+        { label: "14 Hrs - 140 Kms", price: "22000" },
+        { label: "16 Hrs - 160 Kms", price: "24000" },
+        { label: "24 Hrs - 240 Kms", price: "27000" }
+      ],
+      day: [
+        { label: "12 Hrs", price: "15000" },
+        { label: "24 Hrs", price: "23000" },
+        { label: "Fuel Charge", price: "6 Km / 1 Litre" },
+        { label: "Chauffeur Allowance", price: "1000" },
+        { label: "Extra Hr", price: "1000" }
+      ],
+      outstation: [
+        { label: "Km limit / day", price: "400" },
+        { label: "Per Km Cost", price: "100" },
+        { label: "Chauffeur Allowance", price: "1500" }
+      ]
+    }
+  },
+  {
+    name: "Audi Q7",
+    image: "cars/Audi Q7.avif",
+    tariffs: {
+      regular: [
+        { label: "4 Hrs - 40 Kms", price: "10000" },
+        { label: "8 Hrs - 80 Kms", price: "20000" },
+        { label: "10 Hrs - 100 Kms", price: "23000" },
+        { label: "12 Hrs - 120 Kms", price: "25000" },
+        { label: "14 Hrs - 140 Kms", price: "27000" },
+        { label: "16 Hrs - 160 Kms", price: "29000" },
+        { label: "24 Hrs - 240 Kms", price: "35000" }
+      ],
+      day: [
+        { label: "12 Hrs", price: "20000" },
+        { label: "24 Hrs", price: "30000" },
+        { label: "Fuel Charge", price: "6 Km / 1 Litre" },
+        { label: "Chauffeur Allowance", price: "1500" },
+        { label: "Extra Hr", price: "1000" }
+      ],
+      outstation: [
+        { label: "Km limit / day", price: "400" },
+        { label: "Per Km Cost", price: "120" },
+        { label: "Chauffeur Allowance", price: "2000" }
+      ]
+    }
+  },
+  {
+    name: "INNOVA CRYSTA",
+   image: "cars/Innova crysta.avif", // replace with actual image path
+    tariffs: {
+      regular: [
+        { label: "4 Hrs - 40 Kms", price: "2500" },
+        { label: "8 Hrs – 80 Kms", price: "3900" },
+        { label: "12 Hrs – 120 Kms", price: "4500" },
+        { label: "Extra KM", price: "20" },
+        { label: "Extra Hr", price: "200" },
+        { label: "8Hr-12Hr Chauffeur Allowance", price: "500" }
+      ],
+      day: [
+        { label: "12 Hrs", price: "2500" },
+        { label: "12Hr Chauffeur Allowance", price: "500" },
+        { label: "24 Hrs", price: "3500" },
+        { label: "24Hr Chauffeur Allowance", price: "500" },
+        { label: "Fuel", price: "8 Km / 1 Litre" }
+      ],
+      outstation: [
+        { label: "Kms Limit / Day", price: "450" },
+        { label: "Per Km Cost", price: "18" },
+        { label: "12Hr Chauffeur Allowance", price: "500" },
+        { label: "24Hr Chauffeur Allowance", price: "600" }
+      ]
+    }
+  },
+  {
+    name: "DZIRE / ETIOS",
+     image: "cars/Swift Dezire.avif"
+, 
+    tariffs: {
+      regular: [
+        { label: "4 Hrs - 40 Kms", price: "1200" },
+        { label: "8 Hrs – 80 Kms", price: "2000" },
+        { label: "12 Hrs – 120 Kms", price: "2400" },
+        { label: "Extra KM", price: "12" },
+        { label: "Extra Hr", price: "100" },
+        { label: "8Hr-12Hr Chauffeur Allowance", price: "300" }
+      ],
+      day: [
+        { label: "12 Hrs", price: "1200" },
+        { label: "12Hr Chauffeur Allowance", price: "300" },
+        { label: "24 Hrs", price: "1800" },
+        { label: "24Hr Chauffeur Allowance", price: "500" },
+        { label: "Fuel", price: "10 Km / 1 Litre" }
+      ],
+      outstation: [
+        { label: "Kms Limit / Day", price: "350" },
+        { label: "Per Km Cost", price: "12" },
+        { label: "12Hr Chauffeur Allowance", price: "300" },
+        { label: "24Hr Chauffeur Allowance", price: "400" }
+      ]
+    }
+  }
+];
+
+
+
+
